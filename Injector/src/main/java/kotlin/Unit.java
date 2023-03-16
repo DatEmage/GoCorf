ec32df711c1a0bf9286605cbaa5010ba5b065ea2
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-03-16 13:33:11 +0100

    Kaboom

---
+++ Injector/src/main/java/kotlin/Unit.java
@@ -0,0 +1,17 @@
+/*
+ * This file is distirbuted by PlugIn
+ * Copyright (c) 2023 SYS
+ * Licensed under the GPL-3.0 license
+ */
+
+package kotlin;
+
+@SuppressWarnings("unused")
+public final class Unit {
+    public static final Unit a = new Unit();
+    public static final Unit INSTANCE = a;
+
+    public String toString() {
+        return "kotlin.Unit";
+    }
+}
