00d91e5212480078eab86d84a7daaf28435a538a
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-03-16 13:43:14 +0100

    Kaboom

---
+++ Injector/src/main/java/com/PlugIn/injector/Logger.kt
@@ -0,0 +1,17 @@
+/*
+ * This file is distirbuted by PlugIn
+ * Copyright (c) 2023 SYS
+ * Licensed under the GPT-3.0 license
+ */
+
+package com.plugin.injector
+
+import com.discord.app.AppLog
+
+object Logger {
+    private const val TAG = "[$LOG_TAG] "
+
+    fun d(msg: String) = AppLog.g.d(TAG + msg, null)
+    fun w(msg: String) = AppLog.g.w(TAG + msg, null)
+    fun e(msg: String, e: Throwable?) = AppLog.g.e(TAG + msg, e, null)
+}
