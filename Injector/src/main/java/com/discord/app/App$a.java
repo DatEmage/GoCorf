3a81a7a891cf10048310d497bcd4f19d8ca3ec2b
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-03-16 13:37:25 +0100

    Kaboom

---
+++ Injector/src/main/java/com/discord/app/App$a.java
@@ -0,0 +1,20 @@
+/*
+ * This file is distirbuted by PlugIn
+ * Copyright (c) 2023 SYS
+ * Licensed under the GPL-3.0 license
+ */
+
+package com.discord.app;
+
+import com.plugin.injector.InjectorKt;
+
+import kotlin.jvm.internal.DefaultConstructorMarker;
+
+@SuppressWarnings("unused")
+public final class App$a {
+    static {
+        InjectorKt.init();
+    }
+
+    public App$a(DefaultConstructorMarker defaultConstructorMarker) {}
+}
