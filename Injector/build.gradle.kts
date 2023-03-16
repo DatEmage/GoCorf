69f0d3e3dfd37a4ff18557e5a8e27ee149ed6dd7
DatEmage <96537509+DatEmage@users.noreply.github.com>
2023-03-16 13:26:41 +0100

    Kaboom

---
+++ Injector/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    id("com.aliucord.gradle")
+    id("kotlin-android")
+}
+
+PluginIn {
+    projectType.set(com.plugin.gradle.ProjectType.INJECTOR)
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 30
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs = freeCompilerArgs +
+            "-Xno-call-assertions" +
+            "-Xno-param-assertions" +
+            "-Xno-receiver-assertions"
+    }
+
+    buildFeatures {
+        buildConfig = false
+        viewBinding = true
+    }
+}
+
+dependencies {
+    discord("com.discord:discord:${findProperty("discord_version")}")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.plugin:Hook:fb7bf41")
+}
